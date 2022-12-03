0fd038b9b1a06193378a701a04dc00bb02eb4808
chrjo903 <chrjo903@student.liu.se>
2022-12-03 20:27:05 +0100

    day3 part2 done

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,6 +4,46 @@ use std::io::{self, prelude::*, BufReader};
 
 fn main() -> io::Result<()> {
     let file = File::open("input.in")?;
+    let lines: Vec<_> = BufReader::new(file).lines().map(|s| s.unwrap()).collect();
+
+    let mut count: u32 = 0;
+
+    for i in 0..lines.len() / 3 {
+        let bag1: Vec<_> = lines[3 * i].chars().collect();
+        let bag2: Vec<_> = lines[3 * i + 1].chars().collect();
+        let bag3: Vec<_> = lines[3 * i + 2].chars().collect();
+
+        let mut set1: BTreeSet<char> = BTreeSet::new();
+        let mut set3: BTreeSet<char> = BTreeSet::new();
+
+        for i in bag1 {
+            set1.insert(i);
+        }
+
+        for i in bag3 {
+            set3.insert(i);
+        }
+
+        for i in bag2 {
+            if set1.contains(&i) && set3.contains(&i) {
+                let value: u32 = i as u32;
+                if value > 96 && value < 123 {
+                    count += value - 96;
+                } else {
+                    count += value - 38;
+                }
+                break;
+            }
+        }
+    }
+
+    println!("Count: {}", count);
+
+    Ok(())
+}
+
+fn __main() -> io::Result<()> {
+    let file = File::open("input.in")?;
     let reader = BufReader::new(file);
 
     let mut count: u32 = 0;
