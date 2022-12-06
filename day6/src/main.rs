69c4bb3bd1de4277921b3f70ba31f4d51811bc3d
Christoffer Johannesson <christoffer.m.johannesson@gmail.com>
2022-12-06 13:53:27 +0100

    day6 part2 done

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,10 +1,10 @@
-use std::fs::{read_to_string, File};
+use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 fn main() {
     let file_name: &str = "input.in";
     let _a = part1(file_name);
-    // let _b = part2(file_name);
+    let _b = part2(file_name);
 }
 
 fn part1(file_name: &str) -> io::Result<()> {
@@ -14,7 +14,7 @@ fn part1(file_name: &str) -> io::Result<()> {
 
     let chars: Vec<char> = lines[0].chars().collect();
 
-    let mut last_four: Vec<char> = vec![chars[0], chars[1], chars[2], chars[3]];
+    let mut last_four: Vec<char> = Vec::<char>::from(&chars[0..4]);
 
     let mut count: usize = 0;
     for c in chars {
@@ -32,7 +32,24 @@ fn part1(file_name: &str) -> io::Result<()> {
 }
 
 fn part2(file_name: &str) -> io::Result<()> {
-    let input = include_str!("../input.in");
+    let file = File::open(file_name)?;
+    let lines: Vec<_> = BufReader::new(file).lines().map(|s| s.unwrap()).collect();
+
+    let chars: Vec<char> = lines[0].chars().collect();
+
+    let mut last_14: Vec<char> = Vec::<char>::from(&chars[0..14]);
+
+    let mut count: usize = 0;
+    for c in chars {
+        last_14[count % 14] = c;
+        count += 1;
+
+        if !(1..last_14.len()).any(|i| last_14[i..].contains(&last_14[i - 1])) {
+            break;
+        }
+    }
+
+    println!("{} characters need to be read!", count);
 
     Ok(())
 }
