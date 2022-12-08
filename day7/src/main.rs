28f824217c728a0208cbd643b31e3fc60cd64c3a
Christoffer Johannesson <christoffer.m.johannesson@gmail.com>
2022-12-08 14:37:13 +0100

    day7 part1 done

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::io::{self};
+use std::io;
 
 fn main() {
     let file_name: &str = "test1.in";
@@ -10,54 +10,38 @@ fn main() {
 fn part1(_file_name: &str) -> io::Result<()> {
     let input = include_str!("../input.in");
 
-    let mut dir_count: u64 = 0;
     let mut curr_dirs: Vec<String> = Vec::<String>::new();
+    curr_dirs.append(&mut vec!["/".to_string()]);
     let mut dir_sizes: HashMap<String, u64> = HashMap::new();
+    dir_sizes.insert("/".to_string(), 0);
 
     for line in input.lines() {
-        println!("Line -- {}", line);
         if line.chars().next().unwrap() == '$' {
-            if line.contains("ls") {
-                continue;
-            } else if line.contains("cd ..") {
+            if line.starts_with("$ cd ..") {
                 curr_dirs.pop();
-                println!("Stack: {:?}", curr_dirs);
-            } else if line.contains("cd") {
-                let name_str = &line[5..];
-                let mut dir_name: String = name_str.to_string();
-                dir_name.push_str(format!("_{}", dir_count).as_str());
-                // let dir_name = name.as_str();
-                curr_dirs.append(&mut vec![dir_name]);
-                println!("Stack: {:?}", curr_dirs);
+            } else if line.starts_with("$ cd") && !line.starts_with("$ cd /") {
+                let mut dir_name: String = curr_dirs.last().unwrap().to_string();
+                dir_name.push_str("/");
+                dir_name.push_str(&line[5..]);
+                curr_dirs.append(&mut vec![dir_name.clone()]);
                 dir_sizes.insert(dir_name, 0);
-                // println!("Changed folder to {}", dir_name);
-                dir_count += 1;
             }
-        } else if line.starts_with("dir ") {
-            continue;
-            // println!("Dir: {}", &line[4..]);
-        } else {
+        } else if !line.starts_with("dir ") {
             let mut aa = line.split(" ");
             let file_tuple: (u64, &str) = (
                 aa.next().unwrap().parse::<u64>().unwrap(),
                 aa.next().unwrap(),
             );
 
-            println!("File: {}, size: {}", file_tuple.1, file_tuple.0);
-            print!("File added to hashmaps: ");
             for i in 0..curr_dirs.len() {
-                print!("{}, ", curr_dirs[i]);
                 let sizes = dir_sizes.get_mut(&curr_dirs[i]).unwrap();
                 *sizes += file_tuple.0;
             }
-            println!("");
         }
     }
 
-    println!(" -------- Sum --------");
     let mut sum: u64 = 0;
     for dir in dir_sizes {
-        println!("Dir {} has size: {}", dir.0, dir.1);
         if dir.1 < 100000 {
             sum += dir.1;
         }
