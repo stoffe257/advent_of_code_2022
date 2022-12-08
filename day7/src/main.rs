96d69324c2f387931ce738bc01eb8d10b86e2adf
Christoffer Johannesson <christoffer.m.johannesson@gmail.com>
2022-12-08 15:06:12 +0100

    day7 part2 done

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -2,12 +2,47 @@ use std::collections::HashMap;
 use std::io;
 
 fn main() {
-    let file_name: &str = "test1.in";
-    let _a = part1(file_name);
-    // let _b = part2(file_name);
+    let _a = part1();
+    let _b = part2();
 }
 
-fn part1(_file_name: &str) -> io::Result<()> {
+fn part2() -> io::Result<()> {
+    let dir_sizes = calculate_dir_sizes();
+
+    let total_space: u64 = 70000000;
+    let needed_space: i64 = 30000000;
+
+    let used_space: u64 = *dir_sizes.get("/").unwrap();
+
+    let need_to_clear: u64 = -((total_space - used_space) as i64 - needed_space) as u64;
+
+    let mut best_candidate_size: u64 = u64::MAX;
+    for k in dir_sizes {
+        if k.1 > need_to_clear && k.1 < best_candidate_size {
+            best_candidate_size = k.1;
+        }
+    }
+
+    println!("Size of best directory to delete: {}", best_candidate_size);
+
+    Ok(())
+}
+
+fn part1() -> io::Result<()> {
+    let dir_sizes = calculate_dir_sizes();
+
+    let mut sum: u64 = 0;
+    for dir in dir_sizes {
+        if dir.1 < 100000 {
+            sum += dir.1;
+        }
+    }
+
+    println!("Sum: {}", sum);
+    Ok(())
+}
+
+fn calculate_dir_sizes() -> HashMap<String, u64> {
     let input = include_str!("../input.in");
 
     let mut curr_dirs: Vec<String> = Vec::<String>::new();
@@ -39,14 +74,5 @@ fn part1(_file_name: &str) -> io::Result<()> {
             }
         }
     }
-
-    let mut sum: u64 = 0;
-    for dir in dir_sizes {
-        if dir.1 < 100000 {
-            sum += dir.1;
-        }
-    }
-
-    println!("Sum: {}", sum);
-    Ok(())
+    dir_sizes
 }
